06ba7f1c2aa21261d2729517856ddae86d7d9461
Reverie Wisp <contact@reveriewisp.com>
2019-04-20 01:30:19 -0700

    = clarified

--- src/plugin/Main.java
+++ src/plugin/Main.java
@@ -5,6 +5,11 @@ public class Main
 	public static void main(String[] args)
 	{
 		PluginManager pluginManager = new PluginManager("./plugins/");
-		pluginManager.CallAll("Test input", (str) -> PluginLog.Log("Plugin called! Got output: " + str));
+		pluginManager.CallAll("Test input", Main::Output);
+	}
+	
+	public static void Output(String str)
+	{
+		PluginLog.Log("Plugin called! Got output: " + str);
 	}
 }
